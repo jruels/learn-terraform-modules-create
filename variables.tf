01634ca93f7ca185de6d574859c8138b4ab02b07
Jason Smith <jason@smithss.org>
2023-12-13 12:53:53 -0800

    new region

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "vpc_cidr" {
 variable "vpc_azs" {
   description = "Availability zones for VPC"
   type        = list(string)
-  default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  default     = ["us-west-1a", "us-west-1b"]
 }
 
 variable "vpc_private_subnets" {
